37b00a01cac8e5bede1f78c2ad8867559036ed94
Sebin <sebinsuresh@gmail.com>
2021-06-28 01:49:37 -0500

    bcrypt-hash: allow passing argument for hashing

--- bcrypt-hash.js
+++ bcrypt-hash.js
@@ -7,22 +7,33 @@ const readline = require("readline").createInterface({
   output: process.stdout,
 });
 
-readline.question(
-  "Enter the password you want to hash: ",
-  async (plaintext) => {
+if (process.argv.length > 2) {
+  console.log("Hashed string:");
+  const plain = process.argv[2];
+  const hashFunc = async (plaintext) => {
     const hashed = await bcrypt.hash(plaintext, saltRounds);
     console.log(hashed);
     process.exit(0);
-  }
-);
+  };
+  hashFunc(plain);
+} else {
+  readline.question(
+    "Enter the password you want to hash: ",
+    async (plaintext) => {
+      const hashed = await bcrypt.hash(plaintext, saltRounds);
+      console.log(hashed);
+      process.exit(0);
+    }
+  );
 
-/* // Hide the password being entered
-const showEntered = false;
+  /* // Hide the password being entered
+  const showEntered = false;
 
-readline._writeToOutput = function _writeToOutput(stringToWrite) {
-  if (showEntered) {
-    readline.output.write(stringToWrite);
-  } else {
-    readline.output.write("*");
-  }
-}; */
+  readline._writeToOutput = function _writeToOutput(stringToWrite) {
+    if (showEntered) {
+      readline.output.write(stringToWrite);
+    } else {
+      readline.output.write("*");
+    }
+  }; */
+}
